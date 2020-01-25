2aeb208c8289b42585e47069d6d92114fd7e4bfe
Nish Lingam <nish@nishs-mbp.home>
2020-01-25 17:51:10 +0000

    Added components from porfolio-site

---
+++ src/Components/HomeDescription.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomeDescription = () => (
+    <div className = 'home-description'>
+        <div className = 'hello'>
+            Hello,
+        </div>
+        <div>
+            My name is Nish, a Junior Developer and LSE Mathematics graduate.
+        </div>
+        <button className = 'see-work'>
+            SEE WORK
+        </button>
+    </div>
+)
+
+export default HomeDescription;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NavBar = () => (
+    <div className = 'nav-bar'>
+        <div> 
+            Nish Lingam
+        </div>
+        <div className = 'nav-bar-right'>
+            <div className = 'home-button'>
+                Home
+            </div>
+            <div className = 'portfolio-button'>
+                Portfolio
+            </div>
+        </div>
+      
+    </div>
+)
+
+export default NavBar;
