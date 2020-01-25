19d45fbedb8139916b2c653486a967cee4d268ec
Nish Lingam <nish@nishs-mbp.home>
2020-01-25 18:14:58 +0000

    Updated default create react app.js

---
+++ src/Components/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Laptop from '../images/laptopImage.jpg'
+
+
+const Hero = () => (
+
+    <img src= {Laptop} />
+)
+
+export default Hero;
---
+++ src/Components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './Hero';
+import HomeDescription from './HomeDescription';
+import NavBar from './NavBar';
+
+const HomePage = () => (
+    <div>
+        <Hero />
+        <HomeDescription />
+        <NavBar />
+    </div>
+)
+
+export default HomePage;
