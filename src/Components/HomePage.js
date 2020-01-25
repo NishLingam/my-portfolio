895e2a43cdd6be56b2966df595f2ee2ccb749233
Nish Lingam <nish@nishs-mbp.home>
2020-01-25 21:36:54 +0000

    Photo Carousel Implemented

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Hero from './Hero';
 import HomeDescription from './HomeDescription';
 import NavBar from './NavBar';
+import PhotoGallery from './PhotoGallery';
 
 const HomePage = () => (
     <div>
         <Hero />
+        <PhotoGallery />
     </div>
 )
 
--- src/Components/PhotoGallery.js
+++ src/Components/PhotoGallery.js
@@ -1,3 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MemoCubePic from '../images/MemoCube.png';
+import TapMathPic from '../images/TapMath.png';
+import Dicksons from '../images/Dicksons.png';
 
-const photoArray = ['../images/MemoCube.png', '../images/Dicksons.png', '../images/TapMath.png']
+const photoArray = [MemoCubePic, TapMathPic, Dicksons];
+const photoLength = photoArray.length;
+
+
+
+
+
+const PhotoGallery = () => {
+
+    const [photoIndex, setPhotoIndex] = useState(0);
+
+    const photoIndexIncreaser = () => {
+            setPhotoIndex(photoIndex + 1)
+    };
+
+    const photoIndexDecreaser = () => {
+        if (photoIndex === 0) {
+            setPhotoIndex(photoLength - 1)
+        } else {
+            setPhotoIndex(photoIndex - 1)   
+        }
+    };
+
+    return (
+        <div>
+            <h1> Photo Gallery </h1>
+            <div className = 'photo-gallery-carousel'>
+                <button onClick = {photoIndexDecreaser}> Left </button> 
+                <img src={photoArray[photoIndex%photoLength]} alt='Ennaanana' className = 'photo-gallery-main' />
+                <button onClick={photoIndexIncreaser} > Right </button>
+            </div>
+        </div>
+
+    )
+}
+
+
+export default PhotoGallery;
