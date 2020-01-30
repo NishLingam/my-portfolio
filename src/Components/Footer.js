41390a528cf843d70ef0174cc3a9748046bee1ab
Nish Lingam <nish@nishs-mbp.lan>
2020-01-30 22:31:20 +0000

    Added footer SVGs

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { ReactComponent as EmailSVG} from '../images/gmail.svg';
+import { ReactComponent as GitHubSVG} from '../images/github.svg'
 
 const Footer = () => {
 
@@ -11,8 +12,14 @@ const Footer = () => {
     return (
         <div className = 'footer'> 
             <p className='contact-header'> Contact Me </p>
-            <p onClick = {mailToHandler}> Email: nitharshanlingam@gmail.com </p>
-            <a href='https://github.com/NishLingam' target = "_blank" >GitHub: NishLingam </a> 
+            <div onClick = {mailToHandler} className = 'footer-detail'> 
+                <EmailSVG className = 'footer-icon'/>
+                nitharshanlingam@gmail.com 
+            </div>
+            <div className = 'footer-detail'>
+                <GitHubSVG className = 'footer-icon'/>
+                GitHub: NishLingam 
+            </div> 
             <p> Copyright © 2020 Nish Lingam - All Rights Reserved. </p>
         </div>
     )
